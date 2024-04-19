77c27cf4385dacff0f21455a1cc715db8e4a7306
Micah Kennedy <144722193+micahk828@users.noreply.github.com>
2024-04-19 14:16:46 -0400

    Update micah.js

--- Micah/micah.js
+++ Micah/micah.js
@@ -9,3 +9,44 @@ window.addEventListener('load', () => {
     document.body.style.overflow = 'initial';
   }, 1000);
 });
+const coinSfx = new Sfx('sfx/Mario Coin.wav');
+const pipeSfx = new Sfx('sfx/Mario Pipe.wav');
+const powerUpSfx = new Sfx('sfx/Mario Powerup.wav');
+const container = document.querySelector('.content');
+let ninja;
+
+function Sfx(src){
+  this.sound = document.createElement('audio');
+  this.sound.src = src;
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function(){
+    this.sound.play();
+  };
+}
+function Ninjas(name, age, height, color){
+  this.name = name;
+  this.age = age;
+  this.height = height;
+  this.color = color;
+
+  this.coin = () => {
+    //audio
+    coinSfx.play();
+
+    const button = document.querySelector('#btnCoin');
+
+    button.style.setProperty('background-color', '#2ecc71');
+    button.style.setProperty('border-color', '#2ecc71');
+    button.style.setProperty('color', 'var(--bg-color)');
+
+    //coin effect
+    const coinImg = document.createElement('img');
+    coinImg.setAttribute('src', 'img/coin.png');
+    coinImg.setAttribute('id', 'coin-img');
+    coinImg.style.transform = 'translate(-50%,calc(-100% - ' + ninja.height + 'px)';
+    container.appendChild(coinImg);
+  }
+}
