39db0d63e77cd2a5103793ccdc2a00ea35be4dae
NATALYP29 <144722471+NATALYP29@users.noreply.github.com>
2024-04-19 14:26:16 -0400

    Update Nataly.js

--- Nataly/Nataly.js
+++ Nataly/Nataly.js
@@ -9,3 +9,83 @@ window.addEventListener('load');
    document.body.style.overflow = 'initial';
  }, 1000);
 });
+
+const coinSfx = new Sfx('sfx/Mario Coin.wav');
+const pipeSfx = new Sfx('sfx/Mario Pipe.wav');
+const powerUpsfx = new Sfx('sfx/Mario Powerup.wav');
+const container = document.querySelector('.content');
+let ninja;
+
+function Sfx(src) {
+  this.sound = document.createElement('audio');
+  this.sound.src = src;
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  };
+}
+
+function Ninjas(nmae, age, height, color) {
+  this.name = name;
+  this.age = age;
+  this.height = height;
+  this.color = color;
+
+  this.coin = () => {
+    //audio
+    coinSfx.play();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
